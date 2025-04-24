4d7058dbef6b65f8b4d3eaad63ca6f8387ee96aa
olksy <olksyzatyliuk@gmail.com>
2025-04-24 14:54:35 +0300

    fixed export issues

--- database/migrations/2025_04_24_114758_add_number_to_users_table.php
+++ database/migrations/2025_04_24_114758_add_number_to_users_table.php
@@ -4,17 +4,14 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
-        Schema::create('images', function (Blueprint $table) {
-            $table->id();
-            $table->string("path");
-            $table->timestamps();
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('number')->unique()->after('email');
         });
     }
 
@@ -23,6 +20,8 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('images');
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('number');
+        });
     }
 };
